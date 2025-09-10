8871d046619a59927673ccd18b4721067aa9380e
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-10 13:39:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app= FastAPI()  #creates the app instance
 app.mount("/static", StaticFiles(directory="static", name="static"))   #the /static is the url path prefix. the local static folder is mounted onto the browser side and the browser makes requests like /static/homepage.js and this maps the program to look into the local static folder (directory) for homepage.js. the name parameter is an optional parameter for internal reference to a particular static folder if multiple static folders are there...it does not affect the url.
 
 templates= Jinja2Templates(directory: "templates")
-@app.get("/
-
+@app.get("/chat", response_class= HTMLResponse)
+async def chat(request: Request):
+      return templates.TemplateResponse("chatWindow.html",{"request": request})
 
