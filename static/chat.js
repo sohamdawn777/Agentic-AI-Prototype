5dde7c68fad092e0855a463c93e18b07fe071bb0
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-10 14:15:22 +0530

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,7 +1,5 @@
 window.onload = async () => {
 
-//Asking the User for Microphone Access
-
 try {
 const voice= await navigator.mediaDevices.getUserMedia({
 audio: true, video: false });
@@ -10,18 +8,15 @@ catch (error) {
 console.log(error);
 }
 
-//Web Speech API Setup (Voice--->Text)
+const SpeechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition; 
 
-const SpeechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition; //some browsers expose the web speech api directly or some with the webkit prefix...the || sign is bitwise OR so if direct Web Speech is found exposed it uses that otherwise it falls back to the webkit prefix fallback
+const recognition= new SpeechRecognition(); 
 
+recognition.lang= "en-US"; 
 
-const recognition= new SpeechRecognition(); //Creating A Web Speech API voice recognition class
+recognition.interimResults= true; 
 
-recognition.lang= "en-US"; //the language code of the voice to be recognized
-
-recognition.interimResults= true; //
-
-recognition.continuous= true; //does not let the voice capturing stop after one sentence
+recognition.continuous= true; 
 
 let text; 
 
