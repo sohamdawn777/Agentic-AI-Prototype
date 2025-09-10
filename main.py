894a2c2c0a45a60627d13de59de8e7e7caa85b5a
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-10 13:16:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from fastapi import FastAPI, Request, Body
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+app=FastAPI()
