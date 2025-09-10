b3746bba84998ec5db492f1de37af93ca9b07d3f
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-10 13:16:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI, Request, Body
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
