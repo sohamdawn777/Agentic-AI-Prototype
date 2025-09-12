9b3678630eb2ed44d6ccd82c28e1d8d0598f7f20
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-12 17:46:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,3 +48,6 @@ def chat():
     data=request.get_json()
     AIresponse= agent.run(data["query"])
     return jsonify({"resp": AIresponse})
+if __name__=="__main__":
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)    
