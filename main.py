0be855f0986719ef21f313b2674b57808f798ded
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-12 15:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import AgentType, Tool
+from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.memory import ConversationBufferMemory 
 import os
 
