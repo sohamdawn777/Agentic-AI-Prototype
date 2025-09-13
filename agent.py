207e4a61a3839d661491bef90b174d18caf3ebb6
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-13 18:14:31 +0530

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,34 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.memory import ConversationBufferMemory 
+import os
+
+gemini_key=os.getenv("GOOGLE_API_KEY")
+
+llm= ChatGoogleGenerativeAI(model= "gemini-2.5-flash", api_key=gemini_key)
+
+def summary(input):
+    prompt=f"Summarize the full text given below in 2 to 3 sentences\n\n{input}"
+    return llm.invoke(prompt).content
+
+def sentimentAnalysis(input):
+    prompt = f"Analyze the sentiment of the following text. Reply with Positive, Negative, or Neutral:\n\n{input}"
+    return llm.invoke(prompt).content
+
+def toneAnalysis(input):
+    prompt=f"Detect the tone (formal, casual, nervous, confident, etc.) of this text:\n\n{input}"
+    return llm.invoke(prompt).content
+
+def advice(input):
+    prompt=f"Based on this text, give one short motivational advice:\n\n{input}"
+    return llm.invoke(prompt).content
+
+tool1= Tool(name="summary", func=summary, description="This summarizes the user query.")
+tool2= Tool(name="sentimentAnalysis", func=sentimentAnalysis, description="This analyses the sentiments of the user query.")
+tool3= Tool(name="toneAnalysis", func=toneAnalysis, description="This analyses the tone of the user query.")
+tool4= Tool(name="advice", func=advice, description="This gives a short advice to the user.")
+tools= [tool1, tool2, tool3, tool4]
+
+memory= ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+agent=initialize_agent(tools=tools, llm=llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True)
