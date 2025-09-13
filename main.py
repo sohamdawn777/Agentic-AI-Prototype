20881a6d849a97e9bc0a0314fb43315b013b2e4d
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-13 13:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,10 +50,13 @@ def chat():
         AIresponse= agent.run(data["query"])
         return jsonify({"resp": AIresponse})
     except Exception as e:
-        AIresponse= agent.run(data["query"])
-        return jsonify({"resp": AIresponse})
-    return jsonify({"resp": "Your Prompt could not be understood."})    
-        
+        try:
+            queryCleaned= agent.run(f"Understand and summarize this query with your own nuance and understanding: \n{data['query']}")
+            AIresponse= agent.run(queryCleaned)
+            return jsonify({"resp": AIresponse})
+        except Exception as e:
+            return jsonify({"resp": "Your response could not be understood"})    
+    
 if __name__=="__main__":
     port=int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port=port)    
