4a2131680ece4f072fa5c1deb68b102176865dcd
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-13 17:54:54 +0530

    Update homepage.js

--- static/homepage.js
+++ static/homepage.js
@@ -1,7 +1,8 @@
 async function send(event) {
 let userQuery= document.getElementById("text").value;
 let dataSent= await fetch("/query", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({"query": userQuery})});
-let dataReceived= await 
+let dataReceived= await dataSent.json();
+document.getElementById("textPlace").textContent+=`${dataReceived}<br>`;
 }
 
 document.getElementById("b1").addEventListener("click", send);
