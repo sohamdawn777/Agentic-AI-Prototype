7e5866b6965306010a724b75bb16f42ee80b8a99
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-13 23:40:21 +0530

    Update routing.py

--- routing.py
+++ routing.py
@@ -1,6 +1,6 @@
 class Routing:
-    def __init__(self, agentInstance):
-        self.agentInstance=agentInstance
+    def __init__(self, agentInstance1, agentInstance2):
+        self.agentInstance1=agentInstance1
     def run_instance(self, query):
         self.agentInstance.run_agent(query)
         
