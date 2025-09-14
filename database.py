fdc74156bc480ec0982abde79226b520ea9ea120
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 19:25:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 import requests
 
 class Database:
-    def __init__(self, project, key):
-        self.url=f"https://firestore.googleapis.com/v1/projects/{project}/databases/(default)/documents/whispers?key={key}     
-    def store(self, uid):  
-        data={"uid":uid, "memory_instance":
-        requests.post(self.url,headers={"Content-Type":"application/json"},json=data)
+    def __init__(self, project, key, memory):
+        self.url=f"https://firestore.googleapis.com/v1/projects/{project}/databases/(default)/documents" 
+        self.key=key    
+    def store(self, uid): 
+        urL=f"{self.url}/memoryDB/{uid}?key={self.key}"
+        msgs=[]
+        for j in self.memory.chat_memory.messages:
+            msgs.append({"mapValue":{"fields":{"role":{"stringValue":j.type}, "message":{"stringValue":j.content}}}})
+        data={"fields":{"uid":{"stringValue":uid}, "memory_instance":{"arrayValue":{"values":msgs}}}}
+        requests.patch(urL,headers={"Content-Type":"application/json"},json=data)
+                
