60b1b6a5471cdd661d171c48c64eb5cf5a6434ae
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 18:29:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import requests
 
 class Database:
-    def __init__(self, project):
-        self.url=f"https://firestore.googleapis.com/v1/projects/{project}/databases/(default)/documents/whispers?key=AIzaSyD5jRoFS0zvIr4pLE7I-nw00J-uRcwWkUw"      
+    def __init__(self, project, key):
+        self.url=f"https://firestore.googleapis.com/v1/projects/{project}/databases/(default)/documents/whispers?key={key}     
     def store(self, uid):  
         data={"uid":uid, "memory_instance":
         requests.post(self.url,headers={"Content-Type":"application/json"},json=data)
