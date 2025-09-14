42ad5decfa68928ec11499e793d8024c73badb2a
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 13:54:54 +0530

    Update routing.py

--- routing.py
+++ routing.py
@@ -15,7 +15,7 @@ class Routing:
     def route(self, query):    
         router_prompt=PromptTemplate(input_variables=["query"], template="""You are a strict router. Given this user input: {query}. Return exactly one of the following keys (and nothing else): creative_chain, fallback_chain. Answer with only the key name.""")
         router_llm_chain=LLMChain(llm=self.router_model, prompt=router_prompt)
-        decision=router_llm_chain.run({"query":query})
-        print("tichkule tangra redfty: ",decision)
+        #decision=router_llm_chain.run({"query":query})
+      #  print("tichkule tangra redfty: ",decision)
         router_chain=RouterChain.from_chains(destination_chains={"creative_chain":self.wrapperInstance1, "fallback_chain":self.wrapperInstance2}, router_chain=router_llm_chain, default_chain=self.wrapperInstance2, verbose=True)
         return router_chain.run({"query":query})
