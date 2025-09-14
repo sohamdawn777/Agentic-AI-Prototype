c66ea8946318519afe7ca382533bb45c70ec2b89
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 18:00:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,6 @@
 import requests
 
+
+url=f"https://firestore.googleapis.com/v1/projects/{project}/databases/(default)/documents/whispers?key=AIzaSyD5jRoFS0zvIr4pLE7I-nw00J-uRcwWkUw"       
+      requests.post(url,headers={"Content-Type":"application/json"},json=data)
+
