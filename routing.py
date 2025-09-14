b3be191dc1c795d914dead4f0176588b48a5a683
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 11:30:54 +0530

    Update routing.py

--- routing.py
+++ routing.py
@@ -2,18 +2,22 @@ from langchain.chains.router import RouterChain
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
+from agentWrapper import AgentWrapper
 class Routing:
-    def __init__(self, agentInstance1, agentInstance2, gemini_key):
+    def __init__(self, agentInstance1, agentInstance2, gemini_key, query):
         self.agentInstance1=agentInstance1
         self.agentInstance2=agentInstance2
         
-        self.router_model=ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=gemini_key)
-    
-    def _call(self, inputs):
-        query=inputs["query"]
+        wrapperInstance=AgentWrapper(self.agentInstance1, "creative")
+        wrapperInstance({"query":query})
+        
+        
+        
+        router_model=ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=gemini_key)
         
         router_prompt=PromptTemplate(input_variables=["query"], template="Given this user input: {query}, decide which agent should respond: creative or fallback.")
-        router_llm_chain=LLMChain(llm=self.router_model, prompt=router_prompt)
+        self.router_llm_chain=LLMChain(llm=self.router_model, prompt=router_prompt)
         
-        router_chain=RouterChain.from_chains(destination_chains={"creative_chain":self.agentInstance1.run_creative_agent(query), "fallback_chain":self.agentInstance2.run_fallback_agent(query)}, router_chain=router_llm_chain, default_chain=self.agentInstance2.run_fallback_agent(query), verbose=True)
+    def route(self):    
+        router_chain=RouterChain.from_chains(destination_chains={"creative_chain":self.agentInstance1.run_creative_agent(query), "fallback_chain":}, router_chain=self.router_llm_chain, default_chain=self.agentInstance2.run_fallback_agent(query), verbose=True)
     
