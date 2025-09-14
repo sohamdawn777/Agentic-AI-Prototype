a420f73707af12e2ced82664f6eedc4c849e3701
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-14 10:54:21 +0530

    Update agentWrapper.py

--- agentWrapper.py
+++ agentWrapper.py
@@ -1,3 +1,14 @@
-from langchain.chains.base import Chain
-
-class AgentWrapper(Chain):
+ class AgnetWrapper(Chain):   
+     def __init__(agentInstance1, agentInstance2):
+         super().__init__()
+         self.agentInstance1=agentInstance1
+         self.agentInstance2=agentInstance2
+     @property
+     def input_keys(self):
+         return ["query"]
+     @property
+     def output_keys(self):
+         return ["answer"]
+     def _call(self, inputs):
+         query=inputs["query"]
+             
